0d48dd804c88e7026d79733ed457ea03fa00e98c
seo <syh05247@naver.com>
2022-12-29 11:11:16 +0900

    5부 전체적인 데이터 모델링 설계 및 관리자 Category 설계 화면 디자인 구현

---
+++ Ecommerce-Springboot/Library/src/main/java/com/ecommerce/library/service/CategoryService.java
@@ -0,0 +1,17 @@
+package com.ecommerce.library.service;
+
+import com.ecommerce.library.model.Category;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CategoryService {
+
+    List<Category> findAll();
+    Category save(Category category);
+    Optional<Category> findById(Long id);
+    Category update(Category category);
+    void deleteById(Long id);
+    void enabledById(Long id);
+
+}
---
+++ Ecommerce-Springboot/Library/src/main/java/com/ecommerce/library/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,62 @@
+package com.ecommerce.library.service.impl;
+
+import com.ecommerce.library.model.Category;
+import com.ecommerce.library.repository.CategoryRepository;
+import com.ecommerce.library.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    @Autowired
+    private CategoryRepository repo;
+
+    @Override
+    public List<Category> findAll() {
+        return repo.findAll();
+    }
+
+    @Override
+    public Category save(Category category) {
+        Category categorySave = new Category(category.getName());
+        return repo.save(categorySave);
+    }
+
+    @Override
+    public Optional<Category> findById(Long id) {
+        return repo.findById(id);
+    }
+
+    @Override
+    public Category update(Category category) {
+        Category categoryUpdate = new Category();
+        categoryUpdate.setName(category.getName());
+        categoryUpdate.set_activated(category.is_activated());
+        categoryUpdate.set_deleted(category.is_deleted());
+        return repo.save(categoryUpdate);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        Optional<Category> categoryOptional = repo.findById(id);
+        Category category = categoryOptional.get();
+
+        category.set_deleted(true);
+        category.set_activated(false);
+        repo.save(category);
+    }
+
+    @Override
+    public void enabledById(Long id) {
+        Optional<Category> categoryOptional = repo.findById(id);
+        Category category = categoryOptional.get();
+
+        category.set_activated(true);
+        category.set_deleted(false);
+        repo.save(category);
+    }
+}
